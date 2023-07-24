41aa45a186df4a6a77f9a99dfc62aef8319ac57b
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-07-24 16:30:35 +0100

    Try this for eslint disabled

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,9 +2,10 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* eslint-disable */
   if (trueOrFalse) {
-    const task = true; // eslint-disable-line no-unused-vars
-    const task2 = false; // eslint-disable-line no-unused-vars
+    const task = true; 
+    const task2 = false;
   }
 
   return [task, task2];
