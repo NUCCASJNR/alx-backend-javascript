f1ff1bce03cfc0ae86dec93cd6d32ff27a6e2209
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-07-25 18:18:34 +0100

    change dict format

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({
-      status: 200,
-      body: 'success',
-    }))
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error())
     .finally(() => { console.log('Got a message from the API'); });
 }
