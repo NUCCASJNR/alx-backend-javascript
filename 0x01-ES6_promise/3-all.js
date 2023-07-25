1ef95e94bb41b16f12fb51db9bc4cf8b78c16784
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-07-25 16:19:30 +0100

    body, frstname and lastname And Also fixed eslint camel case error

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+  Promise.all([photo, user])
+    .then((results) => {
+      const new_res = results[0];
+      const { body } = new_res;
+      const res = results[1];
+      const first_name = res.firstName;
+      const last_name = res.lastName;
+      console.log(` ${body} ${first_name} ${last_name}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
--- 0x01-ES6_promise/url.js
+++ 0x01-ES6_promise/url.js
@@ -1,21 +1,40 @@
-// Import the node-fetch library
-import fetch from 'node-fetch';
+// // Import the node-fetch library
+// import fetch from 'node-fetch';
 
-function fetchData(url) {
-  return fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok.");
-      }
-      return response.json();
+// function fetchData(url) {
+//   return fetch(url)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok.");
+//       }
+//       return response.json();
+//     });
+// }
+
+// const apiUrl = "https://jsonplaceholder.typicode.com/todos/1";
+// fetchData(apiUrl)
+//   .then((data) => {
+//     console.log("Data fetched:", data);
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
+
+// A function that simulates fetching data based on a name
+function fetchDataByName(names) {
+    return Promise.resolve({ names });
+  }
+  
+  const names = ['Alice', 'Bob', 'Charlie', 'David'];
+  
+  const fetchPromises = Array.from(names, fetchDataByName);
+  console.log(fetchPromises)
+  
+  Promise.all(fetchPromises)
+    .then((results) => {
+      console.log('All data fetched:', results);
+    })
+    .catch((error) => {
+      console.error('One or more promises rejected:', error);
     });
-}
-
-const apiUrl = "https://jsonplaceholder.typicode.com/todos/1";
-fetchData(apiUrl)
-  .then((data) => {
-    console.log("Data fetched:", data);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+  
