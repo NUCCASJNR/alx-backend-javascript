52e97497f9c7b2c409d3b834160e0704fe9a3847
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-07-26 23:20:26 +0100

    New user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  const signup = signUpUser();
-  const upload = uploadPhoto();
-  const promises = [signup, upload, filename];
+  const signup = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(filename);
+  const promises = [signup, upload];
   Promise.allSettled(promises)
     .then((results) => {
       results.forEach((result) => {
---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,2 @@
+export default function loadBalancer(chinaDownload, USDownload) {
+}
