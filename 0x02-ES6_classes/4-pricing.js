21b26b29a535ec2768e99f54dbc1db6f2b3f2a6b
nuccasjnr <adegbitealareef28@yahoo.com>
2023-07-27 20:06:24 +0100

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // setter method for amount
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  // setter method for currency
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('currency must be an instance of currency');
+    }
+  }
+
+  // getter method for amount
+
+  get amount() {
+    return this._amount;
+  }
+
+  // getter method for currency
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (`${amount} * ${conversionRate}`);
+  }
+}
