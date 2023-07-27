e444ba51466e34f99647608e0a71d225f1f5cb5b
nuccasjnr <adegbitealareef28@yahoo.com>
2023-07-27 18:39:43 +0100

    Make student names that passes the eslint check

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  set students(value) {
+    if (Array.isArray(value) && value.every((item) => typeof item === 'string')) {
+      this._students = value;
+    } else {
+      throw new TypeError('Students type must be an array of strings');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
