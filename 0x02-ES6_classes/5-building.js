b1850d72d7782227aad92826845e1b114b42016e
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-07-30 17:39:52 +0100

    Modified task 5 to make task 6 check

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target !== Building) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
