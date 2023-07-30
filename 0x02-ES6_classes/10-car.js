f095edd5ec1e3886cb5ca944caee63d81489e84d
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-07-31 00:41:48 +0100

    this.constructor not this.new

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,41 +1,11 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        this.brandbrand = brand;
-        this.motormotor = motor;
-        this.color = color;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    // Getter method for brand
-    get brand() {
-        return this._brand;
-    }
-
-    // Getter method for motor
-    get motor() {
-        return this._motor;
-    }
-
-    // Getter method for brand
-    get color() {
-        return this._color;
-    }
-
-    // Setter method for brand
-    set brand(value) {
-        this._brand = value;
-    }
-
-    // Setter method for motor
-    set motor(value) {
-        this._motor = value;
-    }
-
-    // Setter method for color
-    set color(value) {
-        this._color = value;
-    }
-
-    cloneCar(motor, color, brand) {
-        return new Car(motor, color, brand)
-    }
+  cloneCar() {
+    return new this.constructor();
+  }
 }
