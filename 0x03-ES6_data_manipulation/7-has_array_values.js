a511b52778d01ffa5a48fd86ccf2801b9ee8f8e9
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-01 18:10:17 +0100

    Changed boolean placement

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -2,11 +2,11 @@
 
 const hasValuesFromArray = (set, array) => {
   for (const ele of array) {
-    if (set.has(ele)) {
-      return true;
+    if (!set.has(ele)) {
+      return false;
     }
   }
-  return false;
+  return true;
 };
 
 export default hasValuesFromArray;
