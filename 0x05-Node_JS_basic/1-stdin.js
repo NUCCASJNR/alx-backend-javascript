4e07442077d7ca7d20e2623bedee83b2f839cefe
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-18 20:47:21 +0100

    changes

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,13 +2,13 @@
 
 // Prints to stdin
 const handleUserInput = (input) => {
-  console.log(`Your name is: ${input}`);
+  process.stdout.write(`Your name is: ${input}`);
 };
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (input) => {
   handleUserInput(input);
 });
-process.on('end', () => {
-  console.log('This important software is now closing\n');
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
 module.exports = handleUserInput;
