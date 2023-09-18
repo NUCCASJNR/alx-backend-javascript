7676f083bbd9a1507cfa2c7c303740be778b66b7
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-18 20:49:22 +0100

    App

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,17 +2,17 @@
 
 // HTTP Server
 
-const app = require('http');
+const http = require('http');
 const port = 1245;
 const DisplayMessage = 'Hello Holberton School!';
 const HostName = 'localhost';
 
-const server = app.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(`${DisplayMessage}\n`);
 });
 
-server.listen(port, HostName, () => {
+app.listen(port, HostName, () => {
 });
-module.exports = server;
+module.exports = app;
