b4638da07fe3e5a8cff331e19af39037988614d9
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-18 21:00:05 +0100

    Added Server msg

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,14 +4,16 @@
 
 const http = require('http');
 const port = 1245;
+const DisplayMessage = 'Hello Holberton School!';
 const HostName = 'localhost';
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  res.end(`${DisplayMessage}`);
 });
 
 app.listen(port, HostName, () => {
+  console.log(`Server listening at http://${HostName}:${port}`);
 });
 module.exports = app;
