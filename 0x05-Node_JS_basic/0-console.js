df861192f5140482f4b62fda204a80951c601fef
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-18 18:01:58 +0100

    console

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,6 +3,7 @@
 // Prints to STDOUT
 
 const displayMessage = (arg) => {
-  process.stdout.write(`${arg}\n`);
+  // process.stdout.write(`${arg}\n`);
+  console.log(`${arg}\n`);
 };
 module.exports = displayMessage;
