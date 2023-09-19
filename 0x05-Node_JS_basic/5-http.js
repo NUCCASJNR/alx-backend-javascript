22a64b94cf7a88e00ce61acb2eb1a177fc78f63e
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-19 14:58:36 +0100

    fix err

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -22,13 +22,13 @@ const app = http.createServer((req, res) => {
           csStudentsCount: responseObject.csStudentsCount,
           sweStudentsCount: responseObject.sweStudentsCount,
           csStudentsList: responseObject.csStudentsList,
-          sweStudentsList: responseObject.sweStudentsList,
+          sweStudentsList: responseObject.sweStudentsList
         };
         res.end(
           `This is the list of our students
-Number of students: ${final.totalStudents}\n`
-          + `Number of students in CS: ${final.csStudentsCount}. List: ${final.csStudentsList}\n`
-          + `Number of students in SWE: ${final.sweStudentsCount}. List: ${final.sweStudentsList}`,
+Number of students: ${final.totalStudents}\n` +
+          `Number of students in CS: ${final.csStudentsCount}. List: ${final.csStudentsList}\n` +
+          `Number of students in SWE: ${final.sweStudentsCount}. List: ${final.sweStudentsList}`
         );
       })
       .catch((error) => {
