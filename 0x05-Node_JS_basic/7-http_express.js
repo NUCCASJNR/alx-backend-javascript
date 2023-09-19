6cc68a15f4f349aac1c8a7e1e72c22555baa3093
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-19 17:34:21 +0100

    More Complex express HTTP

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,47 @@
+#!/usr/bin/node
+
+/* http server using express
+* @author: Al-Areef
+* */
+
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const CSV_FILE = process.argv[2];
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.status(200);
+  res.end('Hello Holberton School!');
+});
+app.get('/students', (req, res) => {
+  countStudents(CSV_FILE)
+    .then((result) => {
+      res.statusCode = 200;
+      const resultString = JSON.stringify(result);
+      const responseObject = JSON.parse(resultString);
+      const final = {
+        totalStudents: responseObject.totalStudents,
+        csStudentsCount: responseObject.csStudentsCount,
+        sweStudentsCount: responseObject.sweStudentsCount,
+        csStudentsList: responseObject.csStudentsList,
+        sweStudentsList: responseObject.sweStudentsList,
+      };
+      res.end(
+        `This is the list of our students
+Number of students: ${final.totalStudents}\n`
+          + `Number of students in CS: ${final.csStudentsCount}. List: ${final.csStudentsList}\n`
+          + `Number of students in SWE: ${final.sweStudentsCount}. List: ${final.sweStudentsList}`,
+      );
+    })
+    .catch((error) => {
+      res.statusCode = 500;
+      res.end(`Internal Server Error: ${error}`);
+    });
+});
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+module.exports = app;
