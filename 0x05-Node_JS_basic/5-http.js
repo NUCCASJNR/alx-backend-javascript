651c6a410b126c6046af9ff124c513d250350476
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-19 13:34:17 +0100

    Express.js Server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,20 +1,75 @@
 #!/usr/bin/node
 
 // HTTP Server
-const countStudents = require('./2-read_file');
-countStudents("database.csv")
+const CSV_FILE = process.argv[2]
 const http = require('http');
+const fs = require('fs');
 
+const countStudents = (CsvFilePath) => {
+  let count = 0;
+  return new Promise((resolve, reject) => {
+    fs.readFile(CsvFilePath, 'utf-8', (error, csvData) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const rows = csvData.split('\n');
+        const headerRow = rows[0].split(',');
+        const fieldIndex = headerRow.indexOf('field');
+        const firstNameIndex = headerRow.indexOf('firstname');
+        if (fieldIndex !== -1) {
+          const csStudents = [];
+          const sweStudents = []; // To store the first names of SWE students
+          for (let i = 1; i < rows.length; i += 1) {
+            if (rows[i].length > 0) {
+              count += 1;
+              const row = rows[i].split(',');
+              if (row[fieldIndex] === 'SWE') {
+                sweStudents.push(row[firstNameIndex]);
+              }
+              if (row[fieldIndex] === 'CS') {
+                csStudents.push(row[firstNameIndex]);
+              }
+            }
+          }
+          const result = {
+            totalStudents: count,
+            csStudentsCount: csStudents.length,
+            sweStudentsCount: sweStudents.length,
+            csStudentsList: csStudents.join(', '),
+            sweStudentsList: sweStudents.join(', '),
+          };
+          resolve(result);
+          console.log(`Number of students: ${result.totalStudents}`);
+          console.log(`Number of students in CS: ${result.csStudentsCount}. List: ${result.csStudentsList}`);
+          console.log(`Number of students in SWE: ${result.sweStudentsCount}. List: ${result.sweStudentsList}`);
+        }
+      }
+    });
+  });
+};
 const port = 1245;
 const HostName = 'localhost';
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-
+    try {
+      const result = await countStudents(CSV_FILE); // Wait for the Promise to resolve
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(`This is the list of our students\n${result.toString()}`);
+    } catch (error) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Internal Server Error');
+    }
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
   }
 });
 
---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+/* http server using express
+* @author: Al-Areef
+* */
+
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.status(200);
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
