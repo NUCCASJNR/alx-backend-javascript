925afa10ff38fd98ead519a12e1790abfa4581bd
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-22 04:01:18 +0100

    logg

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -7,6 +7,6 @@ const Utils = require('./utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  return (`The total is: ${result}`);
+  console.log(`The total is: ${result}`);
 };
 module.exports = sendPaymentRequestToApi;
