74a4844de6421d81d8e6414eabc2be0cf342adc1
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-22 10:12:14 +0100

    API integration

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -5,14 +5,14 @@
  */
 
 const express = require('express');
+
 const port = 7865;
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
   res.status(200);
   res.end('Welcome to the payment system');
 });
-
 
 app.listen(port, () => {
   console.log('API available on localhost port 7865 ');
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,11 +5,25 @@
  */
 
 const request = require('request');
-const chai = require('chai');
+const { expect } = require('chai');
 
 describe('API integration test', () => {
   const API = 'http://localhost:7865';
-  request.get(`${API}/`, (err, res, body) => {
-    chai.expect(res.statusCode).to.be.equal(200);
-  })
-})
+  it ('GET / returns the correct response', (done) => {
+    request.get(`${API}/`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('API integration test', () => {
+  const APIURL = 'http://localhost:7865/err';
+  it ('GET /err return err', (done) => {
+    request.get(`${APIURL}`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+});
