bb1e5f2a7ddd29778028d0b21ff38419e4d3b250
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-22 09:50:04 +0100

    Annotation

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
-/*
+/**
+*
 * handles Payments tokens
+* @param {boolean} success - API request status
  */
 /* eslint-disable no-unused-vars */
 const getPaymentTokenFromAPI = (success) => new Promise((resolve, reject) => {
   if (success) {
     resolve({ data: 'Successful response from the API' });
+  } else {
+    resolve(null);
   }
 });
 module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -8,5 +8,8 @@ const request = require('request');
 const chai = require('chai');
 
 describe('API integration test', () => {
-  const API = 'http://'
+  const API = 'http://localhost:7865';
+  request.get(`${API}/`, (err, res, body) => {
+    chai.expect(res.statusCode).to.be.equal(200);
+  })
 })
