7dd15fc875429587ff17f5515e7d001f1ed26e10
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-22 05:30:45 +0100

    payment token

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+* handles Payments tokens
+ */
+/* eslint-disable no-unused-vars */
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, reject) => {
+  if (success) {
+    resolve({ data: 'Successful response from the API' });
+  }
+});
+module.exports = getPaymentTokenFromAPI;
---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+/*
+* Async payment test
+ */
+
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const chai = require('chai');
+const {expect} = require("chai");
+
+describe('Payment token', function () {
+  it('should test payment API', function (done) {
+    const success = true;
+    getPaymentTokenFromAPI(success)
+      .then((data) => {
+        expect(data).to.be.include;
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
