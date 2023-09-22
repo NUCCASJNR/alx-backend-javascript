84529008eaa40f18cdeae8609912092c4165b7c5
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-22 03:36:15 +0100

    change

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,22 +4,22 @@
 * Test payment integration
  */
 
-const assert = require('assert')
+const assert = require('assert');
 const sinon = require('sinon');
+const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
-const {expect} = require("chai");
 
-describe('Send Payment Request to API', function () {
-  it('should test payment gateway', function () {
+describe('send Payment Request to API', () => {
+  it('should test payment gateway', () => {
     const paymentSpy = sinon.spy(Utils.calculateNumber);
     sendPaymentRequestToApi(100, 20);
     expect(paymentSpy.calledWith(100, 20)).to.equal(false);
   });
 });
 
-describe('Test Utils not called once', function () {
-  it('should test payment gateway', function () {
+describe('test Utils not called once', () => {
+  it('should test payment gateway', () => {
     const paymentSpy = sinon.spy(Utils.calculateNumber);
     sendPaymentRequestToApi(100, 20);
     assert.equal(paymentSpy.calledOnce, false);
