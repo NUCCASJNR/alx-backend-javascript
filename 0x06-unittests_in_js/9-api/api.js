3a5074805ff06339e6d33ff404d0110c3c3b0d7d
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-22 11:27:35 +0100

    Advanced API integration that requires Regex

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+/*
+*Api
+ */
+
+const express = require('express');
+
+const port = 7865;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200);
+  res.end('Welcome to the payment system');
+});
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const { id } = req.params;
+  const result = 'Payment methods for cart';
+  res.send(`${result} : ${id}`);
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865 ');
+});
+module.exports = app;
